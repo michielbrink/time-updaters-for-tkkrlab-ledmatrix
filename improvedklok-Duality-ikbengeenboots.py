6572f38f33c4a66df77412ce296701c46eda6c49
Robert <201292@live.nl>
2014-09-02 19:13:12 +0200

    improved

--- improvedklok-Duality-ikbengeenboots.py
+++ improvedklok-Duality-ikbengeenboots.py
@@ -2,6 +2,6 @@ import os
 import subprocess
 import time
 while(1):
-	time.sleep(0.1);
+	time.sleep(0.350);
 	output = subprocess.check_output(["date", "+%T"]);
 	os.system("wget --spider 'http://10.42.2.104/~jawsper/led.php?action=text&text=<B0075> Ik ben geen boots.      '" + output)
